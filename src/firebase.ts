3d0c63d4ecb223865e6c102e91594a0c3cbe36c3
ISHAN RAJ SINGH <srajishan21@gmail.com>
2026-04-18 21:41:36 +0530

    Add files via upload

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+
+  databaseURL: "",
+
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getDatabase(app);
